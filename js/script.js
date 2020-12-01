f5736e0660b0d96bcf6fe7e826b83027b62e6247
luca cavicchioli <luca.cavicchioli.88@gmail.com>
2020-12-01 18:18:06 +0100

    bonus

--- js/script.js
+++ js/script.js
@@ -9,20 +9,40 @@
 // Creare una select con i seguenti generi: pop, rock, metal e jazz.
 // In base a cosa scegliamo nella select vedremo solo i corrispondenti cd.
 
+
+// X BONUS VEDI QUA https://vuejs.org/v2/guide/forms.html
+
 var app = new Vue ({
   el: '#app',
   data: {
     albumList: [],
-    selectedTags: [],
+    //per il bonus faccio un array di generi e uso selected per il genere attivo
+    arrayGeneri: [], //voglio riempirlo con i generi degli album in albumList
+    genereSelezionato: ''// genere selezionato di default è vuoto, così riesco a prendere tutti gli album nella prima option che h lasciato con value normale e fuori dal v-for
   },
+
   //importo i 10 album
   mounted: function () {
     axios.get('https://flynn.boolean.careers/exercises/api/array/music')
     .then(risposta => {
-      this.albumList = risposta.data.response;;
+      let album = risposta.data.response
+      this.albumList = album;
+
+      //ero partito dividendo la selezione del genere dalla funzione qua sopra, ma mi ritrovavo a copiare la prima parte quindi conviene incorporarla
+
+      // per ogni elemento (lo chiamo album) presente nell'array albumList faccio girare la seguente arrowfunction
+      this.albumList.forEach((album) => {
+        //devo popolare l'array composto dai generi musicali di cui fanno parte gli album in albumList
+
+        //condizione if l'ho messa perchè senza funziona lo stesso, ma non mettendola il select v-model mi dava una opzione di genere per ogni album presente, dandomi dunque doppioni di opzioni
+        if (this.arrayGeneri.includes(album.genre)) {
+          //se il genere dell'album che sto guardando in questo ciclo è già nell'array dei generi NON FACCIO NIENTE
+        } else {
+          //else, quindi se non è già presente, lo aggiungo nell'arraydei generi
+          this.arrayGeneri.push(album.genre);
+        }
+      });
     })
-  },
+  }
 
 })
-
-// genre
